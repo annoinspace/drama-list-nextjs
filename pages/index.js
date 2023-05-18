22128de2011c755971ea90ba426bc2465da1d0ef
annoinspace <aneesahalmask@gmail.com>
2023-05-18 14:02:45 +0100

    layout with children for each page

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head"
 import Image from "next/image"
 import { Inter } from "next/font/google"
 import styles from "@/styles/Home.module.css"
+import Link from "next/link"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -10,6 +11,7 @@ export default function Home() {
     <div>
       <h1>Homepage</h1>
       <p>some placeholder text again</p>
+      <Link href="/dramas">See All Drama Listings</Link>
     </div>
   )
 }
